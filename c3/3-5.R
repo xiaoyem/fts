3cd7cee38a5b83f85a601688cdae418fa95ec1d7
Doriszhaotutu <761725461@qq.com>
2015-08-13 13:28:45 +0800

    Chapter 3-5~14

--- c3/3-5.R
+++ c3/3-5.R
@@ -1,31 +1,19 @@
-#
-# Copyright (c) 2015 by Yuchao Zhao, Xiaoye Meng.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-
 require(xts)
 require(fGarch)
-
-da = read.table("data/m-intc7308.txt", header = T)
-lrtn = log(1 + da[, 2])
-plot(xts(lrtn, order.by = as.Date(paste(substr(da[, 1], 1, 4), substr(da[, 1], 5, 6), substring(da[, 1], 7),
-	sep = '-'))), type = 'l', main = '', xlab = 'date', ylab = 'lrtn')
-m1 = garchFit(~ garch(1, 1), data = lrtn, trace = F)
+da = read.table("m-intc7308.txt", header = T)
+head(da)
+rtn=as.numeric(as.character(da[2:433,2]))
+intc=log(rtn+1)
+at=intc-mean(intc)
+Box.test(intc,lag=12,type='Ljung')
+Box.test(at^2,lag=12,type='Ljung')
+acf(at)
+acf(at^2)
+acf(abs(at))
+pacf(at^2)
+m1 = garchFit(~ garch(1, 1), data = intc, trace = F)
 summary(m1)
-m2 = garchFit(~ garch(1, 1), data = lrtn, cond.dist = 'std', trace = F)
+m2 = garchFit(~ garch(1, 1), data = intc, cond.dist = 'std', trace = F)
 summary(m2)
 Box.test(m2@residuals, lag = 10, type = 'Ljung')
 predict(m2, 5)
-
