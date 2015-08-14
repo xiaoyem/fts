5d9c9563568a9b4834bdace99c23eff09ff47100
Xiaoye Meng <mengxiaoye@dce.com.cn>
2015-08-14 21:36:17 +0800

    improve 3-5, 3-6, 3-7

--- c3/3-6.R
+++ c3/3-6.R
@@ -1,18 +1,37 @@
+#
+# Copyright (c) 2015 by Yuchao Zhao, Xiaoye Meng.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 require(xts)
 require(fGarch)
-da=read.table("m-mrk4608.txt",head=T)
-head(da)
-dim(da)
-mrk=as.numeric(as.character(da[2:752,2]))
-head(mrk)
-lnm=log(mrk+1)
-acf(lnm)
-Box.test(lnm,lag=12,type='Ljung')
-at=lnm-mean(lnm)
-Box.test(at^2,lag=12,type='Ljung')
-acf(at)
-acf(at^2)
-acf(abs(at))
-pacf(at^2)
-m1=garchFit(~arma(0,1)+garch(3,0),data=lnm,trace=FALSE)
-summary(m1)
+
+da = read.table("data/m-mrk4608.txt", header = T)
+mrk = log(1 + da[, 2])
+plot(xts(mrk, order.by = as.Date(paste(substr(da[, 1], 1, 4), substr(da[, 1], 5, 6), substring(da[, 1], 7),
+	sep = '-'))), type = 'l', main = '', xlab = 'date', ylab = 'mrk')
+Box.test(mrk, lag = 12, type = 'Ljung')
+par(mfrow = c(2, 1))
+acf(mrk)
+pacf(mrk)
+m1 = arima(mrk, order = c(0, 0, 1))
+m1
+Box.test(m1$residuals,     lag = 10, type = 'Ljung')
+Box.test(m1$residuals ^ 2, lag = 12, type = 'Ljung')
+par(mfrow = c(1, 1))
+pacf(m1$residuals ^ 2)
+m2 = garchFit(~ arma(0, 1) + garch(3, 0), data = mrk, trace = F)
+summary(m2)
+
--- c3/3-7.R
+++ c3/3-7.R
@@ -1,22 +1,43 @@
+#
+# Copyright (c) 2015 by Yuchao Zhao, Xiaoye Meng.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 require(xts)
 require(fGarch)
-da = read.table("m-3m4608.txt", header = T)
-head(da)
-dim(da)
-rtn=as.numeric(as.character(da[2:756,2]))
-intc=log(rtn+1)
-at=intc-mean(intc)
-Box.test(intc,lag=12,type='Ljung')
-Box.test(intc,lag=6,type='Ljung')
-Box.test(at^2,lag=12,type='Ljung')
-pacf(at^2)
-m1 = garchFit(~ garch(2, 0), data = intc, trace = F)
+require(rugarch)
+
+da = read.table("data/m-3m4608.txt", header = T)
+mmm = log(1 + da[, 2])
+plot(xts(mmm, order.by = as.Date(paste(substr(da[, 1], 1, 4), substr(da[, 1], 5, 6), substring(da[, 1], 7),
+	sep = '-'))), type = 'l', main = '', xlab = 'date', ylab = 'mmm')
+Box.test(mmm,     lag = 12, type = 'Ljung')
+Box.test(mmm ^ 2, lag = 6,  type = 'Ljung')
+Box.test(mmm ^ 2, lag = 12, type = 'Ljung')
+pacf(mmm ^ 2)
+m1 = garchFit(~ arma(0, 0) + garch(2, 0), data = mmm, trace = F)
 summary(m1)
-intc2=intc[1:750]
-m2 = garchFit(~ garch(2, 0), data = intc2, trace = F)
+mmma = mmm[1:750]
+m2 = garchFit(~ arma(0, 0) + garch(2, 0), data = mmma, trace = F)
 summary(m2)
 predict(m2, 5)
-source("garchM.R")
-m3=garchM(intc)
-source("Egarch.R")
-m4=Egarch(intc)
+m3 = ugarchfit(ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(2, 0)),
+	mean.model = list(armaOrder = c(0, 0), archm = T)), mmm)
+show(m3)
+m4 = ugarchfit(ugarchspec(variance.model = list(model = "eGARCH", garchOrder = c(1, 1)),
+	mean.model = list(armaOrder = c(0, 0))), mmma)
+show(m4)
+ugarchforecast(m4, n.ahead = 5)
+
